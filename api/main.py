18adf35fff9de3cf44551314fdc96409fb79cef1
Henintsoa Rajaonarivony <tsutsuraj@gmail.com>
2026-09-05 12:23:35 +0200

    Add CORS middleware to allow browser access from the web frontend

--- api/main.py
+++ api/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 import requests
 import os
 from urllib.parse import urljoin
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 ICECAST_URL = os.getenv("ICECAST_URL")
 stats_json = urljoin(ICECAST_URL, "/status-json.xsl")
+
+
 
 @app.get("/now-playing")
 def now_playing():
