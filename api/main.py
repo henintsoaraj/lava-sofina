a63ddc9832f884080ae03db1f3c89ad9c8b13bc1
Henintsoa Rajaonarivony <tsutsuraj@gmail.com>
2026-09-14 23:28:06 +0200

    Add music upload route to API

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException, status
 import requests
 import os
 from urllib.parse import urljoin
 from fastapi.middleware.cors import CORSMiddleware
+import shutil
+from pathlib import Path
 
 
 app = FastAPI()
@@ -14,7 +16,9 @@ app.add_middleware(
 )
 ICECAST_URL = os.getenv("ICECAST_URL")
 stats_json = urljoin(ICECAST_URL, "/status-json.xsl")
-
+# Dossier où les fichiers importés seront enregistrés
+UPLOAD_DIR = Path("/music")
+MIME_INTERDITS = ["image/png", "image/jpeg", "text/plain", "application/pdf", "video/mp4"]
 
 
 @app.get("/now-playing")
@@ -28,4 +32,27 @@ def now_playing():
     return {
         "title": title,
         "listener": listener
+    }
+
+@app.post("/upload/")
+async def importer_fichier(file: UploadFile = File(...)):
+    extension_valide = file.filename.lower().endswith(".mp3")
+    mime_suspect = file.content_type in MIME_INTERDITS
+
+    if not extension_valide or mime_suspect:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Le fichier doit obligatoirement être au format MP3."
+        )
+    # 1. Définir le chemin de destination avec le nom d'origine du fichier
+    destination = UPLOAD_DIR / file.filename
+    
+    # 2. Ouvrir le fichier de destination et y copier le contenu du fichier reçu
+    with open(destination, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    return {
+        "message": "Fichier importé avec succès !",
+        "nom_fichier": file.filename,
+        "type_contenu": file.content_type
     }
